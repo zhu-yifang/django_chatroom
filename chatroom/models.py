ef2b0f27cf5a8bf366904806d12d71ec5fc65068
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-15 22:54:21 -0800

    Add ChatRoom and Message models

--- chatroom/models.py
+++ chatroom/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
