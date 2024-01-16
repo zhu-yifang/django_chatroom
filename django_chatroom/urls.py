8af143300343c42568365a07fb8898715a4fb622
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-15 23:03:37 -0800

    Refactor URL routing in django_chatroom/urls.py

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from chatroom import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.chat_room_list, name="chat_room_list"),
+    path("", include("chatroom.urls")),
 ]
