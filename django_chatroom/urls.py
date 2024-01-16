cd780b7bac6411e7150f5da43ae6607191a01a7e
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-15 23:02:34 -0800

    Add chat room list view to URL patterns

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chatroom import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.chat_room_list, name="chat_room_list"),
 ]
