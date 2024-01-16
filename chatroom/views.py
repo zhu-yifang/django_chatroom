94e7a36553223874d6b3f484e2e3918b84da0038
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-15 22:58:10 -0800

    Add chat room views

--- chatroom/views.py
+++ chatroom/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import ChatRoom, Message
 
-# Create your views here.
+def chat_room_list(request):
+    rooms = ChatRoom.objects.all()
+    return render(request, 'chat_room_list.html', {'rooms': rooms})
+
+def chat_room(request, room_id):
+    room = ChatRoom.objects.get(id=room_id)
+    messages = Message.objects.filter(room=room).order_by('-timestamp')
+    return render(request, 'chat_room.html', {'room': room, 'messages': messages})
