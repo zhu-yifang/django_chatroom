6c167d9994e9253d7a0560462b520a218ef29b9c
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 19:07:18 -0800

    Add user registration functionality

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,7 +27,7 @@ urlpatterns = [
         "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
     ),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
-    # path("register/", ),
+    path("register/", user_views.register, name="register"),
 ]
 
 
