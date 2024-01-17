0cc7cf3b85c4d24276a8c8b76df30f93d035964e
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 23:52:00 -0800

    Update login template path

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatroom.urls")),
     path(
-        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+        "login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"
     ),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
     path("register/", user_views.register, name="register"),
