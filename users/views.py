3e2c2e7e09af574e0ac3cfc3bad5e3e1b4dd72b6
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:46:11 -0800

    Add user registration functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # Log the user in
+            return redirect("home")  # Redirect to a home page or other appropriate page
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
