c2551e155945aef32b4e0745cbcf738a9bb38e6b
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:37:31 -0800

    Add Profile model with new fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to="profile_pics", blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    last_seen = models.DateTimeField(null=True, blank=True)
+
+    # New fields
+    online_status = models.BooleanField(
+        default=False
+    )  # Indicates if the user is currently online
+
+    def __str__(self):
+        return self.user.username
+
+    # Additional methods as needed
