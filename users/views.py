f2713d37d308a7c68a8976951a3770fe2da36498
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 20:10:54 -0800

    Update UserCreationForm to UserRegisterForm in register view

--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)  # Log the user in
@@ -14,5 +14,5 @@ def register(request):
             messages.success(request, f"Account created for {username}!")
             return redirect("chat_room_list")  # Redirect to a home page or other appropriate page
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
