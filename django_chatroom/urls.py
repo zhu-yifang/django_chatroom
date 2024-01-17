c385f1864f2c213be33e4fb8ee9edefdcbef1698
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:35:34 -0800

    Add static file serving for development environment

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatroom.urls")),
-    path("login/", include("users.urls")),
-    path("logout/", include("users.urls")),
-    path("register/", include("users.urls")),
+    # path("login/", ),
+    # path("logout/", ),
+    # path("register/", ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
