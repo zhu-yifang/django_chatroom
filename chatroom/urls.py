c9f635aea13fe4b194d02c8203b87cef12d70746
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 17:30:13 -0800

    Add create room functionality

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("rooms/", views.chat_room_list, name="chat_room_list"),
     path("room/<int:room_id>/", views.chat_room, name="chat_room"),
+    path("create/", views.create_room, name="create_room"),
 ]
--- chatroom/views.py
+++ chatroom/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ChatRoom, Message
+from .forms import ChatRoomForm
+
 
 def chat_room_list(request):
     rooms = ChatRoom.objects.all()
-    return render(request, 'chat_room_list.html', {'rooms': rooms})
+    return render(request, "chat_room_list.html", {"rooms": rooms})
+
 
 def chat_room(request, room_id):
     room = ChatRoom.objects.get(id=room_id)
-    messages = Message.objects.filter(room=room).order_by('-timestamp')
-    return render(request, 'chat_room.html', {'room': room, 'messages': messages})
+    messages = Message.objects.filter(room=room).order_by("-timestamp")
+    return render(request, "chat_room.html", {"room": room, "messages": messages})
+
+
+def create_room(request):
+    if request.method == "POST":
+        form = ChatRoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("chat_room_list")
+    else:
+        form = ChatRoomForm()
+    return render(request, "create_room.html", {"form": form})
