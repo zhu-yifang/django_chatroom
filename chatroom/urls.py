edd5bd3b7952bd8b3d7a2b27c07871d16f99c0f8
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:01:09 -0800

    Remove unused URLs and imports

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -7,11 +6,4 @@ urlpatterns = [
     path("rooms/", views.chat_room_list, name="chat_room_list"),
     path("room/<int:room_id>/", views.chat_room, name="chat_room"),
     path("create/", views.create_room, name="create_room"),
-    # Login and logout URLs using Django's built-in views
-    path(
-        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
-    ),
-    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
-    # Registration URL
-    path("register/", views.register, name="register"),
 ]
