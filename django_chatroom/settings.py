b40f13ee565154191d50aa44b3cbd5598afded1c
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 20:24:44 -0800

    Add crispy_forms to INSTALLED_APPS and set CRISPY_TEMPLATE_PACK to bootstrap4

--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "chatroom",
     "users",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
