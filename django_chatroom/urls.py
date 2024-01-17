a15127bcdc8b863d931e3b8d7f5180977be67859
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:46:08 -0800

    Add login and logout functionality to urls.py

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatroom.urls")),
-    # path("login/", ),
-    # path("logout/", ),
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
     # path("register/", ),
 ]
 
