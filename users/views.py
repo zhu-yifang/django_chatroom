9eb68bc64373e33ecf76de2eb40b01e0d79921ec
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 19:44:23 -0800

    Add message alerts to base.html and display success message after user registration

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib import messages
 
 
 def register(request):
@@ -9,6 +10,8 @@ def register(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)  # Log the user in
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
             return redirect("chat_room_list")  # Redirect to a home page or other appropriate page
     else:
         form = UserCreationForm()
