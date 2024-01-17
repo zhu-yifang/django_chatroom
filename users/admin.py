7097689ecc4c4b686df66d03b85ba5e99923d066
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-16 18:40:47 -0800

    Add ProfileAdmin customization to users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+# Option 1: Simple registration without customization
+# admin.site.register(Profile)
+
+# Option 2: Customized admin view
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'profile_picture')  # Customize the fields to display in the list view
+    search_fields = ('user__username', 'bio')  # Add search functionality
+    list_filter = ('user__is_staff', 'user__is_active')  # Add filters
+
+    # You can add more customization as needed
+
+admin.site.register(Profile, ProfileAdmin)
