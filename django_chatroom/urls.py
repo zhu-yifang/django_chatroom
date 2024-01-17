d69f409500adc7c83e3a936683f18c8ebab72e6c
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-17 00:07:42 -0800

    Update logout view template

--- django_chatroom/urls.py
+++ django_chatroom/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path(
         "login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"
     ),
-    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("register/", user_views.register, name="register"),
 ]
 
