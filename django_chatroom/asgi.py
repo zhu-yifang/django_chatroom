d50385c8a83468b3c865d07cac26c5c0c28053fa
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-17 21:59:34 -0800

    Add WebSocket routing for chatroom

--- django_chatroom/asgi.py
+++ django_chatroom/asgi.py
@@ -11,9 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatroom.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chatroom.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chatroom.routing.websocket_urlpatterns
+        )
+    ),
 })
