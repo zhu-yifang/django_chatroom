b8c70778b6755cb17a2258e2aaed6564db0c3f02
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-17 17:14:05 -0800

    Add login_required decorator to profile view

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -20,5 +21,6 @@ def register(request):
     return render(request, "users/register.html", {"form": form})
 
 
+@login_required
 def profile(request):
     return render(request, "users/profile.html")
