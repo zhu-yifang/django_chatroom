60e5ae765f732abb281ecacbf6e0dbe289e4a043
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-18 14:45:30 -0800

    Add "daphne" to INSTALLED_APPS

--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap4",
     "debug_toolbar",
-    "daphne",
 ]
 
 MIDDLEWARE = [
