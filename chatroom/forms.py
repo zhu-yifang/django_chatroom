e9f14f9e801948aef564c7a1fbb79385d9f4e6eb
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-17 19:55:49 -0800

    Add MessageForm to forms.py

--- chatroom/forms.py
+++ chatroom/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import ChatRoom
 
+
 class ChatRoomForm(forms.ModelForm):
     class Meta:
         model = ChatRoom
-        fields = ['name']
+        fields = ["name"]
+
+
+class MessageForm(forms.Form):
+    message = forms.CharField(label="Message", max_length=1000)
