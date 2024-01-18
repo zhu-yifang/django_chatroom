99fdd066a49bc25eb0b793275cf0a8bf1af7f027
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-17 18:47:12 -0800

    Update profile_picture field in Profile model

--- users/models.py
+++ users/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to="profile_pics", blank=True)
+    profile_picture = models.ImageField(
+        default="default.jpg", upload_to="profile_pics", blank=True
+    )
     bio = models.TextField(max_length=500, blank=True)
     last_seen = models.DateTimeField(null=True, blank=True)
 
