485f8fd98e9f878caf8f32f934017358f4d3d0f1
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-18 17:04:02 -0800

    Refactor ChatMessage model in models.py

--- chatroom/models.py
+++ chatroom/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255)
+
 
 class ChatMessage(models.Model):
     room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
