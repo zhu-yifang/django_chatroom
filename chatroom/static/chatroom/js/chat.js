b66b8f66e9127c9782606e3a8aebab75345d037d
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-18 23:40:52 -0800

    Seperate js to a single .js file

---
+++ chatroom/static/chatroom/js/chat.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const roomId = document.getElementById('chat-data').dataset.roomId;
+    // Connect to websocket
+    const chatSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/chat/' + roomId + '/'
+    );
+    // Receive message from websocket
+    chatSocket.onmessage = function (e) {
+        const data = JSON.parse(e.data);
+        const chatLog = document.querySelector('#chat-log');
+        const newMessage = document.createElement('li');
+        newMessage.innerHTML = `<strong>${data.username}</strong>: ${data.message} <small class="text-muted">${data.timestamp}</small>`;
+        chatLog.appendChild(newMessage);
+        // Print message to console
+        console.log('message received: ' + data.message);
+    };
+
+    // Handle errors
+    chatSocket.onclose = function (e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+
+    const messageInputDom = document.querySelector('#chat-message-input');
+    const sendMessage = function () {
+        const message = messageInputDom.value;
+        chatSocket.send(JSON.stringify({ 'message': message }));
+        console.log('message sent: ' + message);
+        messageInputDom.value = '';
+    };
+
+    // Send message when button is clicked
+    document.querySelector('#chat-message-submit').onclick = function (e) {
+        sendMessage();
+    };
+
+    // Send message when Enter key is pressed
+    messageInputDom.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Prevent default Enter key behavior (like submitting a form)
+            sendMessage();
+        }
+    });
+});
