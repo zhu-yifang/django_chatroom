96ffdc95980c754acf23212848b18d96b4dad03c
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-21 00:34:31 -0800

    Add image resizing functionality to Profile model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -16,4 +17,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    # Additional methods as needed
+    def save(self):
+        super().save()
+
+        # Resize the profile picture if it's too large
+        img = Image.open(self.profile_picture.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profile_picture.path)
