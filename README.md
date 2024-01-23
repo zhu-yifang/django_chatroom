e4ec7f46748f2b3497f46affc0a6311d60d43866
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-22 23:33:25 -0800

    Add password reset functionality

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - [x] Finish the `chat` functionallity
 - [x] Change the `chat` functionallity to use WebSockets
 - [x] Finish the `profile update` functionallity
+- [ ] Finish reset password functionallity
 - [ ] Use class-based views
 - [ ] Write tests
 - [ ] Write comments for functions and classes
--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -152,3 +152,10 @@ LOGIN_URL = "/login/"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
