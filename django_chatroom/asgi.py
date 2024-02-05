1d46183f743f692e432820b15a3211e182e94f73
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-02-04 23:56:00 -0800

    Refactor ASGI configuration in django_chatroom/asgi.py

--- django_chatroom/asgi.py
+++ django_chatroom/asgi.py
@@ -8,18 +8,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-from chatroom.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_chatroom.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+
+from chatroom.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter(
