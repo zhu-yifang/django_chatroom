9366d7be58ce93cea16e0b11866ae32f40e6e378
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-02-05 23:48:29 -0800

    Update database settings to use PostgreSQL

--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -91,10 +91,21 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "chatroom_db",
+        "USER": "chatroom_user",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
